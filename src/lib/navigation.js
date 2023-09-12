2d02adb61b24801a5962902166337c649645e676
Rob Conery <rob@conery.io>
2023-09-12 10:27:47 -0700

    Initial go, hoping to get something up to look at

---
+++ src/lib/navigation.js
@@ -0,0 +1,33 @@
+export const navigation = [
+  {
+    title: 'Introduction',
+    links: [
+      { title: 'Getting started', href: '/' },
+      { title: 'Installation', href: '/docs/installation' },
+    ],
+  },
+  {
+    title: 'Core concepts',
+    links: [
+
+    ],
+  },
+  {
+    title: 'Advanced guides',
+    links: [
+
+    ],
+  },
+  {
+    title: 'API reference',
+    links: [
+
+    ],
+  },
+  {
+    title: 'Contributing',
+    links: [
+
+    ],
+  },
+]
